f0fc2e0a10a30087ec339bc5e32dbbff652cee49
Chloekkk <skdusdl8804@gmail.com>
2022-04-02 18:44:20 +0900

    feat: Update Typo to Storybook

---
+++ src/components/Typography/index.stories.tsx
@@ -0,0 +1,51 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import GlobalThemeProvider from '../../../styles/GlobalThemeProvider';
+import theme from '../../../styles/theme';
+import Typography, { TypoProps } from './index';
+
+export default {
+  title: 'Components/Typography',
+  component: Typography,
+} as ComponentMeta<typeof Typography>;
+
+const Template = ({ children, ...args }: TypoProps) => (
+  <GlobalThemeProvider theme={theme}>
+    <div style={{ width: '50%', height: '40%', marginTop: '0.5rem' }}>
+      <Typography {...args}>{children}</Typography>
+    </div>
+  </GlobalThemeProvider>
+);
+
+export const Sizes = (args: ComponentStory<typeof Typography>) => {
+  const defaultArgs = {
+    color: 'black' as const,
+  };
+  return (
+    <>
+      <Template size="xs" {...defaultArgs} {...args}>
+        xs : Noto Sans KR 12px
+      </Template>
+      <Template size="sm" {...defaultArgs} {...args}>
+        sm : Noto Sans KR 14px
+      </Template>
+      <Template size="md" {...defaultArgs} {...args}>
+        md : Noto Sans KR 16px
+      </Template>
+      <Template size="lg" {...defaultArgs} {...args}>
+        lg : Noto Sans KR 24px
+      </Template>
+    </>
+  );
+};
+
+export const Custom = (args: ComponentStory<typeof Typography>) => {
+  const defaultArgs = {
+    color: 'black' as const,
+    size: 'sm' as const,
+  };
+  return (
+    <Template {...defaultArgs} {...args}>
+      Noto Sans KR
+    </Template>
+  );
+};
