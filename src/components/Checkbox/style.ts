48642e8a9651aa011fc21f6b628c6048b55d9023
sujk912 <aa991122aa@gmail.com>
2022-04-04 15:43:35 +0900

    refactor: Reduce necessary Props

--- src/components/Checkbox/style.ts
+++ src/components/Checkbox/style.ts
@@ -1,56 +1,45 @@
 import styled from '@emotion/styled';
 
 export interface SCheckboxComponentBox {
-  width: number;
-  height: number;
-  padding: string;
-  backgroundColor: string;
-}
-export interface SCheckboxComponentInput {
-  backgroundColor: string;
+  width?: string;
+  height?: string;
 }
 export interface SCheckboxComponentText {
-  fontSize: number;
-  fontColor: string;
+  link?: boolean;
 }
 
 const getCheckboxBox = ({ ...box }) => {
   return `
-    width: ${box.width}px;
-    height: ${box.height}px;
-    padding: ${box.padding};
-    background-color: ${box.backgroundColor};
+    display: flex;
+    flex-basis: auto;
+    align-items: center;
+    width: ${box.width};
+    height: ${box.height};
+    padding: 0.4rem;
+    background-color: #FFFFFF;
   `;
 };
-const getCheckboxInput = ({ ...input }) => {
-  return `
-    accent-color: ${input.backgroundColor};
-  `;
-};
-const getCheckboxText = ({ link = false, ...text }) => {
-  const isFloatRight = () => (link ? 'margin-left: auto;' : '');
+const getCheckboxText = ({ ...text }) => {
+  const isFloatRight = () => (text.link ? 'margin-left: auto;' : '');
   return `
       ${isFloatRight}
-      font-size: ${text.fontSize}px;
-      color: ${text.fontColor};
+      font-size: 0.875rem;
+      color: #4E4E4E;
   `;
 };
 
 export const StyledCheckbox = styled.div<SCheckboxComponentBox>`
-  display: flex;
-  flex-basis: auto;
-  align-items: center;
   ${getCheckboxBox}
 `;
 export const EmptyDiv = styled.div`
   display: flex;
   align-items: center;
 `;
-export const StyledCheckboxInput = styled.input<SCheckboxComponentInput>`
+export const StyledCheckboxInput = styled.input`
   width: 16px;
   height: 16px;
   border-radius: 4px;
-  ${getCheckboxInput};
+  accent-color: #4e4e4e;
 `;
 export const StyledCheckboxText = styled.div<SCheckboxComponentText>`
   padding-left: 1rem;
