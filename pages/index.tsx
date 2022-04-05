0d183b4c2e97eee05b5f125b94543a7e19d6e7e1
sujk912 <aa991122aa@gmail.com>
2022-04-06 02:33:07 +0900

    test: Create NormalSignInModal Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,25 @@
 import SignInModal from '../src/containers/SigninModal';
 =======
 import { SignInModal } from '../src/containers/SignInModal';
+<<<<<<< HEAD
 import { NormalSignupModal } from '../src/containers/NormalSignupModal';
 >>>>>>> 114a5ec (test: Make NormalSignupModal visible in Home)
+=======
+import { NormalSignInModal } from '../src/containers/NormalSignInModal';
+>>>>>>> 697b26d (test: Create NormalSignInModal Page)
 
 const Home = () => {
   return <SignInModal />;
 };
 
+<<<<<<< HEAD
 const NormalSignupModalPage = () => {
   return <NormalSignupModal />;
 };
 
 export default NormalSignupModalPage;
+=======
+const NormalSignInPage = () => <NormalSignInModal />;
+
+export default NormalSignInPage;
+>>>>>>> 697b26d (test: Create NormalSignInModal Page)
