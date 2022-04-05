af49f84be84b10cc6efc106cd0a940fe199e5b9b
hong-soohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2022-04-05 22:55:23 +0900

    style: Delete useless comment in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,7 @@
 import { SignInModal } from '../src/containers/SignInModal';
 
 const Home = () => {
-  return (
-    // <div>
-    //   <Head>
-    //     <title>Create Next App</title>
-    //     <meta name="description" content="Grooteji" />
-    //     <link rel="icon" href="/favicon.ico" />
-    //   </Head>
-
-    //   <main>
-    //     <h1>🌳 그루터기</h1>
-    //   </main>
-    // </div>
-    <SignInModal />
-  );
+  return <SignInModal />;
 };
 
 export default Home;
