99504258d98f2d11b263d57e74cd6d8d7f857d04
sujk912 <aa991122aa@gmail.com>
2022-04-07 13:39:52 +0900

    create: Create Normal Signup Modal

---
+++ src/containers/NormalSignupModal.tsx
@@ -0,0 +1,44 @@
+import Box from '../components/Box';
+import Button from '../components/Button';
+import Checkbox from '../components/Checkbox';
+import Input from '../components/Input';
+import Typography from '../components/Typography';
+import Wrapper from '../components/Wrapper';
+
+export const NormalSignupModal = () => (
+  <Box width={500} height={800}>
+    <Wrapper flexDirection={'row'} rowInterval={20}>
+      <Typography size={'lg'} color={'black'} weight={'BOLD'}>
+        🌳 간편 가입하기
+      </Typography>
+      <Wrapper flexDirection={'column'} rowInterval={15}>
+        <Typography size={'lg'} color={'darkgray'}>
+          인생선배를 찾는 지름길,
+        </Typography>
+        <Typography size={'lg'} color={'darkgray'}>
+          그루터기
+        </Typography>
+      </Wrapper>
+      <Wrapper flexDirection={'column'} interval={20}>
+        <Input width={375} height={40} placeholder={'이메일'} />
+        <Wrapper flexDirection={'row'} columnInterval={1} justifyContent={'flex-end'}>
+          <Button color={'black'} fontColor={'white'} borderColor={'none'} name={'이메일 인증'} size={'md'} />
+        </Wrapper>
+      </Wrapper>
+      <Wrapper flexDirection={'column'} rowInterval={20}>
+        <Input width={375} height={40} placeholder={'비밀번호'}></Input>
+        <Input width={375} height={40} placeholder={'비밀번호 확인'}></Input>
+        <Wrapper flexDirection={'row'} rowInterval={5}>
+          <Checkbox label={'전체 동의'}></Checkbox>
+          <Wrapper flexDirection={'row'} rowInterval={10}>
+            <Checkbox label={'서비스 이용 약관 동의 (필수)'} link={true} paddingLeft={'1rem'}></Checkbox>
+            <Checkbox label={'개인정보 수집 및 이용 동의 (필수)'} link={true} paddingLeft={'1rem'}></Checkbox>
+            <Checkbox label={'만 14세 이상입니다 (필수)'} paddingLeft={'1rem'}></Checkbox>
+            <Checkbox label={'광고성 정보 수신 동의 (선택)'} link={true} paddingLeft={'1rem'}></Checkbox>
+          </Wrapper>
+        </Wrapper>
+      </Wrapper>
+      <Button color={'white'} name={'회원가입하기'} fontColor={'black'} size={'lg'}></Button>
+    </Wrapper>
+  </Box>
+);
