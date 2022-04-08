e8bb2449ee8fc18e3f6097c5a8b6c34be6d041b5
sujk912 <aa991122aa@gmail.com>
2022-04-08 13:30:47 +0900

    refactor: Change NormalSigninModal to export default

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,20 @@ import SignInModal from '../src/containers/SigninModal';
 =======
 import { SignInModal } from '../src/containers/SignInModal';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { NormalSignupModal } from '../src/containers/NormalSignupModal';
 >>>>>>> 114a5ec (test: Make NormalSignupModal visible in Home)
 =======
 import { NormalSignInModal } from '../src/containers/NormalSignInModal';
 >>>>>>> 697b26d (test: Create NormalSignInModal Page)
+=======
+>>>>>>> 58f9965 (refactor: Change NormalSigninModal to export default)
 
 const Home = () => {
   return <SignInModal />;
 };
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 const NormalSignupModalPage = () => {
   return <NormalSignupModal />;
@@ -24,3 +28,6 @@ const NormalSignInPage = () => <NormalSignInModal />;
 
 export default NormalSignInPage;
 >>>>>>> 697b26d (test: Create NormalSignInModal Page)
+=======
+export default Home;
+>>>>>>> 58f9965 (refactor: Change NormalSigninModal to export default)
