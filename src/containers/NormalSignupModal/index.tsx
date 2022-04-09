d504f9d1298d70fb33a4fdabe118fe83ff271d35
hong-soohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2022-04-09 19:42:02 +0900

    fix: Resolve conflick in normal sign-in sign-up modal

--- src/containers/NormalSignupModal/index.tsx
+++ src/containers/NormalSignupModal/index.tsx
@@ -7,11 +7,11 @@ import Wrapper from '../../components/Wrapper';
 
 export const NormalSignupModal = () => (
   <Box width={500} height={800}>
-    <Wrapper flexDirection={'row'} rowInterval={20}>
+    <Wrapper flexDirection={'row'} gap={{ rowGap: 20 }}>
       <Typography size={'lg'} color={'black'} weight={'BOLD'}>
         🌳 간편 가입하기
       </Typography>
-      <Wrapper flexDirection={'column'} rowInterval={15}>
+      <Wrapper flexDirection={'column'} gap={{ rowGap: 15 }}>
         <Typography size={'lg'} color={'darkgray'}>
           인생선배를 찾는 지름길,
         </Typography>
@@ -19,18 +19,18 @@ export const NormalSignupModal = () => (
           그루터기
         </Typography>
       </Wrapper>
-      <Wrapper flexDirection={'column'} interval={20}>
+      <Wrapper flexDirection={'column'} gap={{ gap: 20 }}>
         <Input width={375} height={40} placeholder={'이메일'} />
-        <Wrapper flexDirection={'row'} columnInterval={1} justifyContent={'flex-end'}>
+        <Wrapper flexDirection={'row'} gap={{ columnGap: 1 }} justifyContent={'flex-end'}>
           <Button color={'black'} fontColor={'white'} borderColor={'none'} name={'이메일 인증'} size={'md'} />
         </Wrapper>
       </Wrapper>
-      <Wrapper flexDirection={'column'} rowInterval={20}>
+      <Wrapper flexDirection={'column'} gap={{ rowGap: 20 }}>
         <Input width={375} height={40} placeholder={'비밀번호'}></Input>
         <Input width={375} height={40} placeholder={'비밀번호 확인'}></Input>
-        <Wrapper flexDirection={'row'} rowInterval={5}>
+        <Wrapper flexDirection={'row'} gap={{ rowGap: 5 }}>
           <Checkbox label={'전체 동의'}></Checkbox>
-          <Wrapper flexDirection={'row'} rowInterval={10}>
+          <Wrapper flexDirection={'row'} gap={{ rowGap: 10 }}>
             <Checkbox label={'서비스 이용 약관 동의 (필수)'} link={true} paddingLeft={'1rem'}></Checkbox>
             <Checkbox label={'개인정보 수집 및 이용 동의 (필수)'} link={true} paddingLeft={'1rem'}></Checkbox>
             <Checkbox label={'만 14세 이상입니다 (필수)'} paddingLeft={'1rem'}></Checkbox>
