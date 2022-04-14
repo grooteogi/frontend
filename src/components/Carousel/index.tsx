aa8c58ef581d2ed0253c76403c32980defc6757c
sujk912 <aa991122aa@gmail.com>
2022-04-14 12:37:07 +0900

    refactor: Resolve vercel error

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -34,13 +34,15 @@ const settings = {
 const Carousel = ({ device, imgLists }: CarouselProps) => {
   let idx = 0;
   return (
-    <StyledCarousel device={device} {...settings}>
-      {imgLists.map(({ src, alt, ondbClick }: ImgItem) => (
-        <div key={idx++}>
-          <StyledCarouselItem src={src} onDoubleClick={ondbClick} alt={alt} />
-        </div>
-      ))}
-    </StyledCarousel>
+    <>
+      <StyledCarousel device={device} {...settings}>
+        {imgLists.map(({ src, alt, ondbClick }: ImgItem) => (
+          <div key={idx++}>
+            <StyledCarouselItem src={src} onDoubleClick={ondbClick} alt={alt} />
+          </div>
+        ))}
+      </StyledCarousel>
+    </>
   );
 };
 
