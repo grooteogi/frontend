c5200ecafc7a346ab8e803329c04655b0c931b8b
Chloekkk <skdusdl8804@gmail.com>
2022-04-16 15:00:07 +0900

    feat: Create proxy server with localhost

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/:path*',
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      { source: '/oauth/kakao', destination: '/auth/test', permanent: true },
+      { source: '/oauth/google', destination: '/auth/test', permanent: true },
+    ];
+  },
   reactStrictMode: true,
 };
 
