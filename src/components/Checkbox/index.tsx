99fd77b0b3da6d4104b0cb3a60053b4f4071f2d9
Chloekkk <skdusdl8804@gmail.com>
2022-04-16 15:04:38 +0900

    feat: Update Checkbox checked props

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -10,15 +10,24 @@ import {
 
 interface CheckBoxProps extends SCheckboxComponentBox, SCheckboxComponentText {
   label: string;
+  checked?: boolean;
   onClick?: () => void;
 }
 
-const Checkbox = ({ label, link, width = '100%', height = '30px', paddingLeft, onClick }: CheckBoxProps) => {
+const Checkbox = ({
+  label,
+  link,
+  width = '100%',
+  height = '30px',
+  paddingLeft,
+  onClick,
+  checked = false,
+}: CheckBoxProps) => {
   const box = { width, height, paddingLeft };
   return (
     <StyledCheckbox {...box}>
       <EmptyDiv>
-        <StyledCheckboxInput type={'checkbox'} onClick={onClick} />
+        <StyledCheckboxInput type={'checkbox'} onClick={onClick} checked={checked} />
         <StyledCheckboxText>{label}</StyledCheckboxText>
       </EmptyDiv>
       {link && <StyledCheckboxDetail>보기</StyledCheckboxDetail>}
