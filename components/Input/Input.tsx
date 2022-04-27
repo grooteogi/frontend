6de2e7995d751cb4e4203037021127a73c31ff1a
Chloekkk <skdusdl8804@gmail.com>
2022-04-27 19:32:11 +0900

    fix: Fix Input type error

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -14,12 +14,10 @@ interface InputProps extends TextareaProps {
   type?: 'text' | 'textarea' | 'password';
   value?: string;
   placeholder?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Input: React.FC<InputProps> = ({ type = 'text', value, placeholder, onChange, rows = 5 }) => {
-  if (type == 'textarea')
-    return <Styled.textarea defaultValue={value} placeholder={placeholder} onChange={onChange} rows={rows} />;
   return <Styled.input type={type} defaultValue={value} placeholder={placeholder} onChange={onChange} />;
 };
 
