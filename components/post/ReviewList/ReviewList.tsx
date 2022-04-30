c5d3b953369ed53d2f8ef6eac53e99610fac2871
sujk912 <aa991122aa@gmail.com>
2022-04-30 17:40:35 +0900

    refactor: rebase branch

--- components/post/ReviewList/ReviewList.tsx
+++ components/post/ReviewList/ReviewList.tsx
@@ -1,17 +1,10 @@
-import Wrapper from '../Wrapper';
-import Typography from '../Typography';
+import Wrapper from '../../common/Wrapper';
+import Typography from '../../common/Typography';
 import React from 'react';
 import Styled from './ReviewList.style';
 
 export type Item = {
-<<<<<<< HEAD
-<<<<<<< HEAD
   id: string | number;
-=======
->>>>>>> e090ca8 (refactor : Refactor style)
-=======
-  id: string | number;
->>>>>>> 3cf1d40 (refactor: Change the map key from the idx++ format to the received format)
   userProfilePicSrc: string;
   username: string;
   time: string;
@@ -23,28 +16,11 @@ export interface ReviewListProps {
 }
 
 const ReviewItems: React.FC<ReviewListProps> = ({ reviewList }) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
   if (reviewList.length > 0) {
-    return (
-      <>
-<<<<<<< HEAD
-        {reviewList.map(({ id, userProfilePicSrc, username, time, text }: Item) => (
-          <Wrapper key={id} flexDirection={'column'} padding={{ paddingTop: '26px' }}>
-=======
-        {reviewList.map(({ userProfilePicSrc, username, time, text }: Item) => (
-          <Wrapper key={idx++} flexDirection={'column'} padding={{ paddingTop: '26px' }}>
->>>>>>> e090ca8 (refactor : Refactor style)
-=======
-  if (reviewList.length > 0)
-=======
-  if (reviewList.length > 0) {
->>>>>>> 62bdd5d (refactor: Enclose the if statement using {})
     return (
       <>
         {reviewList.map(({ id, userProfilePicSrc, username, time, text }: Item) => (
           <Wrapper key={id} flexDirection={'column'} padding={{ paddingTop: '26px' }}>
->>>>>>> 3cf1d40 (refactor: Change the map key from the idx++ format to the received format)
             <Wrapper flexDirection={'row'} gap={{ gap: 12 }} justifyContent={'flex-start'} alignItems={'flex-start'}>
               <Styled.profileImg src={userProfilePicSrc} />
               <Wrapper flexDirection={'column'} justifyContent={'space-around'}>
--- components/post/ReviewList/index.stories.tsx
+++ components/post/ReviewList/index.stories.tsx
@@ -11,42 +11,21 @@ export default {
 export const Default = (args: ComponentStory<typeof ReviewList>) => {
   const list = [
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
       id: 1,
-=======
->>>>>>> e090ca8 (refactor : Refactor style)
-=======
-      id: 1,
->>>>>>> 3cf1d40 (refactor: Change the map key from the idx++ format to the received format)
       username: '졸리다',
       userProfilePicSrc: '/imgs/Carousel1.png',
       time: '2022-04-26 12:40',
       text: '대외활동 다수 경험, 공모전 수상 팁들 알려주셨어요. 덕분에 대학생활 제대로 할 수 있을 것 같아요!!!',
     },
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
       id: 2,
-=======
->>>>>>> e090ca8 (refactor : Refactor style)
-=======
-      id: 2,
->>>>>>> 3cf1d40 (refactor: Change the map key from the idx++ format to the received format)
       username: '지금은 새벽 3시',
       userProfilePicSrc: '/imgs/Carousel2.png',
       time: '2022-04-26 12:40',
       text: '대외활동 다수 경험, 공모전 수상 팁들 알려주셨어요. 덕분에 대학생활 제대로 할 수 있을 것 같아요!!!',
     },
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
       id: 3,
-=======
->>>>>>> e090ca8 (refactor : Refactor style)
-=======
-      id: 3,
->>>>>>> 3cf1d40 (refactor: Change the map key from the idx++ format to the received format)
       username: '왜 에러떠',
       userProfilePicSrc: '/imgs/Carousel3.png',
       time: '2022-04-26 12:40',
