b1232a56992034ebf79cace791a8d537231614c2
sujk912 <aa991122aa@gmail.com>
2022-05-14 14:19:56 +0900

    refactor: Fix Error

--- pages/post/detail.tsx
+++ pages/post/detail.tsx
@@ -5,8 +5,10 @@ import ShowSchedule from '@components/post/ScheduleShow';
 import Layout from '@components/post/Layout';
 import MeetingInfo from '@components/post/MeetingInfo';
 import { MeetingInfoType, ReviewType, ScheduleType } from 'types/postDetail';
+import postInfo from '@components/post/detail.mock';
 
-const Post: NextPage = ({ post }: any) => {
+const Post: NextPage = () => {
+  const post = postInfo;
   const { ...meetingInfo }: MeetingInfoType = post;
   const { schedules }: ScheduleType = post;
   const { reviews }: ReviewType = post;
