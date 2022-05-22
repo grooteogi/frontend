fa9c6ddff4659cf7a5e6fb17e713b2a4cdc69321
Chloekkk <skdusdl8804@gmail.com>
2022-05-23 02:57:36 +0900

    refactor: Delete CreateFormik file

--- pages/post/create.tsx
+++ pages/post/create.tsx
@@ -1,19 +1,16 @@
 import { withRouter } from 'next/router';
 import type { NextPage } from 'next';
-import Layout from '@components/post/Layout';
+import Layout from '@components/post/layout';
 import CreateSchedule from '@components/post/ScheduleEdit/ScheduleEdit';
 import MeetingInfoEdit from '@components/post/MeetingInfoEdit/MeetingInfoEdit';
 import StickyBar from '@components/post/StickyBar/StickyBar';
-import CreateFormik from '@components/post/CreateFormik';
 import { PostCreateProvider } from '@components/post/context';
 
 const Create: NextPage = () => {
   return (
     <PostCreateProvider>
       <Layout>
-        <CreateFormik>
-          <MeetingInfoEdit />
-        </CreateFormik>
+        <MeetingInfoEdit />
         <CreateSchedule />
       </Layout>
       <StickyBar buttonName={'약속 생성하기'} />
