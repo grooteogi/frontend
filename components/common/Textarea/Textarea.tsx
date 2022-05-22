e1ddad4bb15b82341ad9003548ea07c615921d3d
Chloekkk <skdusdl8804@gmail.com>
2022-05-23 02:55:57 +0900

    refactor: Refactor form components to render field props

--- components/common/Textarea/Textarea.tsx
+++ components/common/Textarea/Textarea.tsx
@@ -1,19 +1,22 @@
+import { FieldInputProps } from 'formik';
 import React from 'react';
 import Styled, { STextareaProps } from './Textarea.styled';
 
 interface TextareaProps extends STextareaProps {
+  field?: FieldInputProps<string>;
   value?: string;
   placeholder?: string;
   rows?: number;
 }
 
 const Textarea: React.FC<React.TextareaHTMLAttributes<HTMLTextAreaElement> & TextareaProps> = ({
+  field,
   value,
   placeholder,
   rows = 5,
   ...styles
 }) => {
-  return <Styled.textarea defaultValue={value} placeholder={placeholder} rows={rows} {...styles} />;
+  return <Styled.textarea defaultValue={value} placeholder={placeholder} rows={rows} {...styles} {...field} />;
 };
 
 export default Textarea;
