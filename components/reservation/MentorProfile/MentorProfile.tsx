ffd3e9c04d99807c8bb2eb06347c44cc6e45b9e1
Chloekkk <skdusdl8804@gmail.com>
2022-05-23 15:22:21 +0900

    fix: Fix hashtag type err

--- components/reservation/MentorProfile/MentorProfile.tsx
+++ components/reservation/MentorProfile/MentorProfile.tsx
@@ -3,7 +3,7 @@ import Image from '@components/common/Image';
 import Title from '@components/common/Title';
 import Typography from '@components/common/Typography';
 import { useEffect, useRef, useState } from 'react';
-import { fetchedHashtag } from 'types/fetchedHashtag';
+import { HashtagEntity } from 'types/entity';
 import Styled from './MentorProfile.styled';
 
 const MentorProfile = () => {
@@ -14,7 +14,7 @@ const MentorProfile = () => {
   }, []);
 
   const title = '저랑같이 공부해요!';
-  const hashtags: fetchedHashtag[] = [
+  const hashtags: HashtagEntity[] = [
     { id: 1, hashtagType: 'PERSONALITY', tag: '대학생활', registered: '' },
     { id: 2, hashtagType: 'PERSONALITY', tag: '취업준비', registered: '' },
     { id: 3, hashtagType: 'PERSONALITY', tag: '코딩테스트', registered: '' },
