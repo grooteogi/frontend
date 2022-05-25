24fb89938becc1b8003b7e7104c17e99c076436f
Chloekkk <skdusdl8804@gmail.com>
2022-05-25 14:17:37 +0900

    feat: Reservation connect phoneNumber api

--- lib/api/reservation.ts
+++ lib/api/reservation.ts
@@ -1,24 +1,25 @@
+import { ReservationCreateRequestDto } from './../../types/api';
 import { ReservationEntity } from 'types/entity';
 import { postData } from './restApi';
 
 const reservation = {
-  createReservation: async (reservation: ReservationEntity) => {
+  createReservation: async (data: ReservationCreateRequestDto) => {
     const url = '/reservation';
-    const status = await postData(url, reservation);
+    const status = await postData(url, data);
     return status;
   },
   sendMessage: async (phoneNumber: string) => {
-    const url = `/reservation/sms/send/phoneNumber=${phoneNumber}`;
+    const url = `/reservation/sms/send?phoneNumber=${phoneNumber}`;
     const status = await postData(url, {});
     return status;
   },
-  confirmMessage: async (reservation: ReservationEntity) => {
+  confirmMessage: async (data: { phoneNumber: string; code: string }) => {
     const url = '/reservation/sms/check';
-    const status = await postData(url, reservation);
+    const status = await postData(url, data);
     return status;
   },
   resendMessage: async (reservation: ReservationEntity) => {
-    const url = '/reservation/sms/send';
+    const url = '/reservation/send-sms';
     const status = await postData(url, reservation);
     return status;
   },
