2937c3ca5bdd21651903bb3cfdbeab2880335703
sujk912 <aa991122aa@gmail.com>
2022-05-25 14:29:55 +0900

    Fix: Connec Module Error

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -2,7 +2,6 @@ import { useRouter, withRouter } from 'next/router';
 import type { NextPage } from 'next';
 import ReviewList from '@components/post/ReviewList/ReviewList';
 import ShowSchedule from '@components/post/ScheduleShow/ScheduleShow';
-import Layout from '@components/post/layout';
 import MeetingInfo from '@components/post/MeetingInfo/MeetingInfo';
 import postInfo from '@components/post/detail.meetingInfo.mock';
 import schedule from '@components/post/detail.schedule.mock';
@@ -10,6 +9,7 @@ import review from '@components/post/detail.review.mock';
 import StickyBar from '@components/post/StickyBar/StickyBar';
 import post from '@lib/api/post';
 import Link from 'next/link';
+import Layout from '@components/post/layout';
 
 const Detail: NextPage = () => {
   const router = useRouter();
