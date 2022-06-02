feb8efbb767a787e578fa660bd38e9977fcff38c
Chloekkk <skdusdl8804@gmail.com>
2022-06-02 20:17:59 +0900

    feat: Update Like/Post react-query hooks

--- components/post/usePost.tsx
+++ components/post/usePost.tsx
@@ -1,11 +1,32 @@
 import post from '@lib/api/post';
 import { useQuery } from 'react-query';
-import { PostEntity } from 'types/entity';
+import { PostDetailResponseDto } from 'types/response';
 
 const usePost = (postId: string) => {
-  const { isLoading, data, error } = useQuery<PostEntity>(
+  const { isLoading, data, error } = useQuery<PostDetailResponseDto>(
     ['post', postId],
     async () => (await post.getPost(postId)).data,
+    {
+      initialData: {
+        postId: '',
+        title: '',
+        content: '',
+        imageUrl: '',
+        createAt: '',
+        hashtags: [],
+        creditType: 'DIRECT',
+        likes: {
+          count: 0,
+          liked: false,
+        },
+        mentor: {
+          userId: 0,
+          nickname: '',
+          imageUrl: '',
+        },
+        isAuthor: false,
+      },
+    },
   );
   console.log('post', data);
   return { isLoading, postData: data, error };
