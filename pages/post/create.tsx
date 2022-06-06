62893eec06b5a3d7e1cfbca1e25c6e0acfe55a23
Chloekkk <skdusdl8804@gmail.com>
2022-06-06 19:27:10 +0900

    refactor: Refactor post logic

--- pages/post/create.tsx
+++ pages/post/create.tsx
@@ -1,4 +1,4 @@
-import { Router, useRouter, withRouter } from 'next/router';
+import { useRouter, withRouter } from 'next/router';
 import type { NextPage } from 'next';
 import ScheduleEdit from '@components/post/ScheduleEdit/ScheduleEdit';
 import MeetingInfoEdit from '@components/post/MeetingInfoEdit/MeetingInfoEdit';
@@ -10,6 +10,7 @@ import { CreditType } from 'types/enum';
 import useRouteWarning from '@hooks/useRouteWarning';
 import { useState } from 'react';
 import moment from 'moment';
+import { validEmptyData } from '@lib/validator';
 
 const Create: NextPage = () => {
   const [saved, setSaved] = useState(false);
@@ -18,10 +19,13 @@ const Create: NextPage = () => {
   useRouteWarning(saved, '포스트가 저장되지 않았습니다. 정말 나가시겠습니까?');
 
   const handleSubmit = async (data: PostCreateRequestDto) => {
+    if (validEmptyData(data, ['imageUrl'])) {
+      return alert('빈 항목이 있습니다.');
+    }
     const response = await post.createPost(data);
     if (response.status === 200) {
       alert('약속 생성 완료!');
-      router.push('/');
+      router.push(`/post/${response.data.postId}`);
     } else alert('포스트생성 실패!');
   };
 
@@ -32,19 +36,18 @@ const Create: NextPage = () => {
         content: '',
         imageUrl: '',
         hashtags: [],
-        creditType: CreditType['DIRECT' as const],
+        creditType: CreditType['DIRECT' as keyof typeof CreditType],
         schedules: [],
       }}
       onSubmit={async (data: PostCreateRequestDto) => {
         const { schedules, creditType } = data;
-        // TODO: 이거 좀 더 코드 깔끔하게 수정하기
         schedules.forEach(({ startTime, endTime }: { startTime: string; endTime: string }, index: number) => {
           schedules[index].startTime = moment(startTime, 'HH:mm:ss').format('HH:mm:ss');
           schedules[index].endTime = moment(endTime, 'HH:mm:ss').format('HH:mm:ss');
         });
         data.creditType =
           Object.keys(CreditType).find(key => CreditType[key as keyof typeof CreditType] === creditType) ??
-          CreditType['DIRECT' as const];
+          CreditType['DIRECT' as keyof typeof CreditType];
         console.log(data);
         setSaved(true);
         handleSubmit(data);
